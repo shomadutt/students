f36b08128c3250749cb56085b22a42d74041111b
shoma <shomasdutt@gmail.com>
2022-04-07 15:54:00 +0100

    Created home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{'../../bootstrap-5.0.2-dist/css/bootstrap.min.css'}}" rel="stylesheet">
+    <link href="{{'../../css/students.css'}}" rel="stylesheet">
+    <title>Student Database</title>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Student Database</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <!-- <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul> -->
+
+            </div>
+        </div>
+    </nav>
+
+
+    <div id="image"></div>
+
+
+
+
+    <script src="{{'../../bootstrap-5.0.2-dist/css/bootstrap.min.js'}}"></script>
+</body>
+
+</html>
